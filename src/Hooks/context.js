1f1752876a050d056444e960e88365943e285042
Xurd3 <61953663+Xurd3@users.noreply.github.com>
2022-06-08 12:56:21 +0100

    added starter files

---
+++ src/Hooks/context.js
@@ -0,0 +1,19 @@
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AppContext.Provider value={{ user, setUser }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
