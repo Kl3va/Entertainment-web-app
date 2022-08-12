f66b793b6f7f3be2fd3a9eeb11e3d187e6c66c47
Kl3va <ikwunzekelvin@gmail.com>
2022-08-12 13:38:03 +0100

    converted all icons to components for easy styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,27 @@
-import React from "react";
-import styles from "Components/navbar.module.scss";
-import logo from "StarterAssets/assets/logo.svg";
-import iconHome from "StarterAssets/assets/icon-nav-home.svg";
-import iconTv from "StarterAssets/assets/icon-nav-tv-series.svg";
-import iconMovies from "StarterAssets/assets/icon-nav-movies.svg";
-import iconBookmark from "StarterAssets/assets/icon-nav-bookmark.svg";
-import profileImg from "StarterAssets/assets/image-avatar.png";
+import React from 'react'
+import styles from 'Components/navbar.module.scss'
+import logo from 'StarterAssets/assets/logo.svg'
+//import iconHome from "StarterAssets/assets/icon-nav-home.svg";
+import { ReactComponent as IconHome } from 'StarterAssets/assets/icon-nav-home.svg'
+import { ReactComponent as IconTv } from 'StarterAssets/assets/icon-nav-tv-series.svg'
+import { ReactComponent as IconMovies } from 'StarterAssets/assets/icon-nav-movies.svg'
+import { ReactComponent as IconBookmark } from 'StarterAssets/assets/icon-nav-bookmark.svg'
+import profileImg from 'StarterAssets/assets/image-avatar.png'
 
 const Navbar = () => {
   return (
     <aside className={styles.navcontainer}>
-      <img src={logo} alt="logo" className={styles.logo} />
+      <img src={logo} alt='logo' className={styles.logo} />
       <nav>
-        <img src={iconHome} alt="home" className={""} />
-        <img src={iconTv} alt="tv-series" className={""} />
-        <img src={iconMovies} alt="movies" className={""} />
-        <img src={iconBookmark} alt="bookmarks" className={""} />
+        {/*<img src={iconHome} alt="home" className={""} />*/}
+        <IconHome className={styles.svg} />
+        <IconTv className={styles.svg} />
+        <IconMovies className={styles.svg} />
+        <IconBookmark className={styles.svg} />
       </nav>
-      <img src={profileImg} alt="Profile" className={""} />
+      <img src={profileImg} alt='Profile' className={''} />
     </aside>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
