22e6b3d7e0ea3487be21bd2a49e764bd2b22e8d0
Kl3va <ikwunzekelvin@gmail.com>
2022-08-18 16:55:53 +0100

    added protected routes using dummy authentication

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 //import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "Pages/Login/Login";
-import SignUp from "Pages/SignUp/SignUp";
-import Layout from "Pages/Home/Layout";
-import Movies from "Pages/Movies/Movies";
-import Tv from "Pages/Tv/Tv";
-import Bookmark from "Pages/Bookmark/Bookmark";
-import Home from "Pages/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import PrivateRoutes from 'Components/PrivateRoutes'
+import Login from 'Pages/Login/Login'
+import SignUp from 'Pages/SignUp/SignUp'
+import Layout from 'Pages/Home/Layout'
+import Movies from 'Pages/Movies/Movies'
+import Tv from 'Pages/Tv/Tv'
+import Bookmark from 'Pages/Bookmark/Bookmark'
+import Home from 'Pages/Home/Home'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="tv" element={<Tv />} />
-          <Route path="bookmarks" element={<Bookmark />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='movies' element={<Movies />} />
+            <Route path='tv' element={<Tv />} />
+            <Route path='bookmarks' element={<Bookmark />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/PrivateRoutes.jsx
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import React from 'react'
+
+const PrivateRoutes = () => {
+  let auth = { token: false }
+
+  return auth.token ? <Outlet /> : <Navigate to='/login' />
+}
+
+export default PrivateRoutes
