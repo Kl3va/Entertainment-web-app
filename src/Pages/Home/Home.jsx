d2add7c6febdb7175ef3ea6aa4a753283787803d
Kl3va <ikwunzekelvin@gmail.com>
2022-09-12 17:48:15 +0100

    updated movie ui

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
           }) => {
             return (
               <div key={id} className={`${styles.trend} packed`}>
-                <picture className={styles.trend__picture}>
+                <picture className={styles.wrapper__image}>
                   <source media='(min-width: 801px)' srcSet={trending.large} />
                   <img src={trending.small} alt='Top Trending movie' />
                 </picture>
@@ -69,53 +69,60 @@ const Home = () => {
       </div>
       <div className={styles.recommended}>
         <h1 className='heading-large'>Recommended For You</h1>
-        {recommendedMovies.map(
-          ({
-            title,
-            id,
-            thumbnail: { regular },
-            year,
-            category,
-            rating,
-            isBookmarked,
-          }) => {
-            return (
-              <div key={id} className={`${styles.recommend}`}>
-                <div className={`${styles.picture} packed`}>
-                  <picture className={styles.recommend__image}>
-                    <source media='(min-width: 801px)' srcSet={regular.large} />
-                    <source
-                      media='(min-width: 400px)'
-                      srcSet={regular.medium}
-                    />
-                    <img src={regular.small} alt='Top movie' />
-                  </picture>
-                  <div className={styles.icon__wrapper}>
-                    <img src={IconPlay} alt='click to play movie' />
-                    <p>Play</p>
-                  </div>
+        <div className={styles.recommend__grid}>
+          {recommendedMovies.map(
+            ({
+              title,
+              id,
+              thumbnail: { regular },
+              year,
+              category,
+              rating,
+              isBookmarked,
+            }) => {
+              return (
+                <div key={id} className={`${styles.recommend}`}>
+                  <div className={`${styles.picture} packed`}>
+                    <picture className={styles.wrapper__image}>
+                      <source
+                        media='(min-width: 801px)'
+                        srcSet={regular.large}
+                      />
+                      <source
+                        media='(min-width: 400px)'
+                        srcSet={regular.medium}
+                      />
+                      <img src={regular.small} alt='Top movie' />
+                    </picture>
+                    <div className={styles.icon__wrapper}>
+                      <img src={IconPlay} alt='click to play movie' />
+                      <p>Play</p>
+                    </div>
 
-                  <div className={styles.bookmark__wrapper}>
-                    <img
-                      src={isBookmarked ? iconBookmarkFull : iconBookmarkEmpty}
-                      alt='click to bookmark'
-                    />
+                    <div className={styles.bookmark__wrapper}>
+                      <img
+                        src={
+                          isBookmarked ? iconBookmarkFull : iconBookmarkEmpty
+                        }
+                        alt='click to bookmark'
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.category__wrapper}>
+                    <div className={styles.types}>
+                      <p className={styles.year}>{year}</p>
+                      <span></span>
+                      <p className={styles.type}>{category}</p>
+                      <span></span>
+                      <p className={styles.rating}>{rating}</p>
+                    </div>
+                    <h2>{title}</h2>
                   </div>
                 </div>
-                <div className={styles.category__wrapper}>
-                  <div className={styles.types}>
-                    <p className={styles.year}>{year}</p>
-                    <span></span>
-                    <p className={styles.type}>{category}</p>
-                    <span></span>
-                    <p className={styles.rating}>{rating}</p>
-                  </div>
-                  <h2>{title}</h2>
-                </div>
-              </div>
-            )
-          }
-        )}
+              )
+            }
+          )}
+        </div>
       </div>
     </section>
   )
