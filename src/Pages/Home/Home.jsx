24423e30d1b84cf6f666d71e4f9c6af157fa0187
Kl3va <ikwunzekelvin@gmail.com>
2022-09-16 19:01:32 +0100

    updated home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,14 +9,13 @@ import iconBookmarkFull from 'StarterAssets/assets/icon-bookmark-full.svg'
 
 const Home = () => {
   const { movieData } = useGlobalContext()
-  //console.log(movieData)
 
-  //console.log(movieData)
-
+  //Data for trending movies section
   const trendingMovies = movieData.filter((movie) => {
     return movie.thumbnail?.trending
   })
 
+  //Data for recommended movie section
   const recommendedMovies = movieData.filter((movie) => {
     return movie.thumbnail?.regular
   })
@@ -25,47 +24,58 @@ const Home = () => {
     <section className={styles.main}>
       <div className={styles.trending}>
         <h1 className='heading-large'>Trending</h1>
-        {trendingMovies.map(
-          ({
-            title,
-            id,
-            thumbnail: { trending },
-            year,
-            category,
-            rating,
-            isBookmarked,
-          }) => {
-            return (
-              <div key={id} className={`${styles.trend} packed`}>
-                <picture className={styles.wrapper__image}>
-                  <source media='(min-width: 801px)' srcSet={trending.large} />
-                  <img src={trending.small} alt='Top Trending movie' />
-                </picture>
-                <div className={styles.icon__wrapper}>
-                  <img src={IconPlay} alt='click to play movie' />
-                  <p>Play</p>
-                </div>
-
-                <div className={styles.bookmark__wrapper}>
-                  <img
-                    src={isBookmarked ? iconBookmarkFull : iconBookmarkEmpty}
-                    alt='click to bookmark'
-                  />
-                </div>
-                <div className={styles.category__wrapper}>
-                  <div className={styles.types}>
-                    <p className={styles.year}>{year}</p>
-                    <span></span>
-                    <p className={styles.type}>{category}</p>
-                    <span></span>
-                    <p className={styles.rating}>{rating}</p>
+        <div className={`${styles.trend__grid} ${styles.snap__inline}`}>
+          {trendingMovies.map(
+            ({
+              title,
+              id,
+              thumbnail: { trending },
+              year,
+              category,
+              rating,
+              isBookmarked,
+            }) => {
+              return (
+                <div key={id} className={`${styles.trend} packed`}>
+                  <picture className={styles.wrapper__image}>
+                    <source
+                      media='(min-width: 801px)'
+                      srcSet={trending.large}
+                    />
+                    <img
+                      src={trending.small}
+                      alt='Top Trending movie'
+                      className={styles.trend__img}
+                    />
+                  </picture>
+                  <div className={styles.icon__wrapper}>
+                    <img src={IconPlay} alt='click to play movie' />
+                    <p>Play</p>
                   </div>
-                  <h2 className={styles.subheading}>{title}</h2>
+
+                  <div className={styles.bookmark__wrapper}>
+                    <img
+                      src={isBookmarked ? iconBookmarkFull : iconBookmarkEmpty}
+                      alt='click to bookmark'
+                    />
+                  </div>
+                  <div
+                    className={`${styles.category__wrapper} ${styles.category__align}`}
+                  >
+                    <div className={styles.types}>
+                      <p className={styles.year}>{year}</p>
+                      <span></span>
+                      <p className={styles.type}>{category}</p>
+                      <span></span>
+                      <p className={styles.rating}>{rating}</p>
+                    </div>
+                    <h2 className={styles.subheading}>{title}</h2>
+                  </div>
                 </div>
-              </div>
-            )
-          }
-        )}
+              )
+            }
+          )}
+        </div>
       </div>
       <div className={styles.recommended}>
         <h1 className='heading-large'>Recommended For You</h1>
